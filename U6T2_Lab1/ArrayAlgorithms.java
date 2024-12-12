197ac6ef9ca677937c648167ed6138a65d33a35f
brianw3943 <brianw124@nycstudents.net>
2024-12-12 16:44:51 +0000

    commit

--- U6T2_Lab1/ArrayAlgorithms.java
+++ U6T2_Lab1/ArrayAlgorithms.java
@@ -96,6 +96,141 @@ public class ArrayAlgorithms {
     }
     return combined;
   }
+  /** Changes the first and last element of nums to be num
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: nums.length > 0
+   */
+  public static void bookEnd(int[] nums, int num) {
+    nums[0] = num;
+    nums[nums.length - 1] = num;
+}
+ /** Multiplies each number in numList by multiplier.
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static void multiplyBy(int[] numList, int multiplier) {
+    for (int i = 0; i < numList.length; i++) {
+      numList[i] = numList[i] * multiplier;
+    }
+  }
+  /** Returns an array of ints where each element is the product
+   *  of the corresponding element in numList and multiplier.
+   *
+   *  THIS METHOD DOES NOT MUTATE (MODIFY) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static int[] multiplyByNoModify(int[] numList, int multiplier) {
+    int[] multiplied = new int[numList.length];
+    for (int i = 0; i < numList.length; i++) {
+      multiplied[i] = numList[i] * multiplier;
+    }
+    return multiplied;
+  }
+  /** Adds a "!" to the end of each String in wordList.  However, if a
+   *  a string already ends in an exclamation point, do NOT add another.
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: wordList.length > 0
+   */
+  public static void addExclamation(String[] wordList) {
+    for (int i = 0; i < wordList.length; i++) {
+      if (!wordList[i].substring(wordList[i].length() - 1).equals("!")) {
+        wordList[i] += "!";
+      }
+    }
+  }
+  /** Returns an array of booleans where each element reflects whether
+   *  the corresponding element in tempList is at or below the freezing
+   *  temperature of 32 (true) or not (false).
+   *
+   *  THIS METHOD DOES NOT MUTATE (MODIFY) ORIGINAL ARRAY
+   *  PRECONDITION: tempList.length > 0
+   */
+  public static boolean[] isFreezing(int[] tempList) {
+    boolean[] freezing = new boolean[tempList.length];
+    for (int i = 0; i < tempList.length; i++) {
+      if (tempList[i] <= 32) {
+        freezing[i] = true;
+      } else {
+        freezing[i] = false;
+      }
+    }
+    return freezing;
+  }
+  /** Shifts all elements in numList to the LEFT one index
+   *
+   *  The first element in the array should wrap around
+   *  to the back of the array.  For example, given the array
+   *  {5, 1, 3, 4, 7, 6}, this method modifies the array
+   *  to {1, 3, 4, 7, 6, 5} (all elements shifted left one
+   *  with the last element wrapping around to the back)
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static void shiftLeft(int[] numList) {
+    int firstNum = numList[0];
+    for (int i = 1; i < numList.length; i++) {
+      numList[i - 1] = numList[i];
+    }
+    numList[numList.length - 1] = firstNum;
+ }
+  /** Shifts all elements in numList to the RIGHT one index.
+   *
+   *  The last element in the array should wrap around
+   *  to the front of the array.  For example, given the array
+   *  {5, 1, 3, 4, 7, 6}, this method modifies the array
+   *  to {6, 5, 1, 3, 4, 7} (all elements shifted right one
+   *  with the last element wrapping around to the front)
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static void shiftRight(int[] numList) {
+    int lastNum = numList[numList.length - 1];
+    for (int i = numList.length - 1; i >= 1; i--) {
+      numList[i] = numList[i - 1];
+    }
+    numList[0] = lastNum;
+  }
+  /** Reverses all elements in an array.
+   *
+   *  For example, given the array {5, 1, 3, 4, 7, 6}, this method
+   *  modifies the array to {6, 7, 4, 3, 1, 5}.
+   *  Should work with all array lengths (both odd and even).
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static void reverse(int[] numList) {
+    int[] temp = new int[numList.length];
+    for (int i = 0; i < numList.length; i++) {
+      temp[i] = numList[i];
+    }
+    for (int i = 0; i < numList.length; i++) {
+      int ind = numList.length - i - 1;
+      numList[ind] = temp[i];
+    }
+  }
+/** Reverses all elements in an array.
+   *
+   *  For example, given the array {5, 1, 3, 4, 7, 6}, this method
+   *  modifies the array to {6, 7, 4, 3, 1, 5}.
+   *  Should work with all array lengths (both odd and even).
+   *
+   *  THIS METHOD MUTATES (MODIFIES) ORIGINAL ARRAY
+   *  PRECONDITION: numList.length > 0
+   */
+  public static void reverse2(int[] numList) {
+    for (int i = 0; i < numList.length / 2; i++) {
+      int temp = numList[i];
+      int ind = numList.length - i - 1;
+      numList[i] = numList[ind];
+      numList[ind] = temp;
+    }
+  }
 
 
 }
--- U6T2_Lab1/Main.java
+++ U6T2_Lab1/Main.java
@@ -2,24 +2,29 @@ package U6T2_Lab1;
 
 public class Main {
     public static void main(String args[]) {
-        int[] nums1 = {1, 2, 3, 4, 5, 6};
-        int[] nums2 = {10, 11, 12, 13, 14, 15, 16, 17};
-        int[] combined = ArrayAlgorithms.combine(nums1, nums2);
-        for (int i = 0; i < combined.length; i++) {
-            System.out.print(combined[i] + " ");
-        }
-        System.out.println();
-        System.out.println("combined list has " + combined.length + " elements");
-        
-        int[] nums3 = {5, 8, 6, 2};
-        int[] nums4 = {1, 7, 10, 0, 9};
-        int[] combined2 = ArrayAlgorithms.combine(nums3, nums4);
-        for (int i = 0; i < combined2.length; i++) {
-            System.out.print(combined2[i] + " ");
-        }
-        System.out.println();
-        System.out.println("combined list has " + combined2.length + " elements");
-          
+        int[] temps = {32, 35, 40, 31, 29, 33, 50, 27};
+boolean[] result = ArrayAlgorithms.isFreezing(temps);
+for (int i = 0; i < result.length; i++) {
+    System.out.print(result[i] + " ");
+}
+System.out.println();
+// original array is NOT modified
+for (int i = 0; i < temps.length; i++) {
+    System.out.print(temps[i] + " ");
+}
+System.out.println();
+int[] temps2 = {10, 20, 30, 0, -20};
+boolean[] result2 = ArrayAlgorithms.isFreezing(temps2);
+for (int i = 0; i < result2.length; i++) {
+    System.out.print(result2[i] + " ");
+}
+System.out.println();
+int[] temps3 = {40, 45, 50, 60, 55};
+boolean[] result3 = ArrayAlgorithms.isFreezing(temps3);
+for (int i = 0; i < result3.length; i++) {
+    System.out.print(result3[i] + " ");
+}
+
     }
 
 }
